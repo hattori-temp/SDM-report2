ebfda93cbc35b9d44d812d86a5a2db3183197f13
hattori-temp <hattori.kenya@technotut.net>
2026-02-05 20:12:22 +0900

    テストケースの変更

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,23 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+        def test_sample1(self):
+                self.assertEqual (1, calc(1, 1)) # 最小値
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+        def test_sample2(self):
+                self.assertEqual (998001, calc(999, 999)) # 最大値
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+        def test_sample3(self):
+                self.assertEqual (999, calc(1, 999))
+                self.assertEqual (999, calc(999, 1)) # 入れ替え
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+        def test_sample4(self):
+                self.assertEqual (-1, calc(1000, 1))
+                self.assertEqual (-1, calc(1, 1000)) # 負の値
+
+        def test_sample5(self):
+                self.assertEqual (-1, calc(-1, 100))
+
+        def test_sample6(self):
+                self.assertEqual ("error", calc("a", 100)) # 文字列
 
